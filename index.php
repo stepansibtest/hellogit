94d08fe0773954917d7a39524f324ed4aaf670ad
stepan.sib <stepan.sib@gmail.com>
2013-05-29 16:22:03 +0700

    path1 commit 1

--- index.php
+++ index.php
@@ -48,7 +48,10 @@ header('Content-Type: text/html; charset=utf-8');
     <span class="silver">
     Использование оперативной памяти: <?= to_bytes(memory_get_usage()) ?><br/>
     Пиковое значение использования оперативной памяти: <?= to_bytes(memory_get_peak_usage()) ?>
+    <br/>Path1: Some changes
     </span>
+
+
 </div>
 
 </body>
